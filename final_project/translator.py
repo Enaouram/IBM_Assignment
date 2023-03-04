31f44eb7d22eed6f109b6a062897b5d46c04679a
Enaouram <99275409+Enaouram@users.noreply.github.com>
2023-03-04 18:53:22 +0100

    Update translator.py

--- final_project/translator.py
+++ final_project/translator.py
@@ -1,1 +1,11 @@
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+authenticator = IAMAuthenticator('BLCXCACU5xg4NVKKvPVlYAQcM_mFk_DzJfLHzRNSoCL1')
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url('https://api.au-syd.language-translator.watson.cloud.ibm.com/instances/572872b6-5ffc-408f-891f-939ec6bb0242')
 
