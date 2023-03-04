aec61e8680f7a03ae8a6006f4fca037d60805816
Enaouram <99275409+Enaouram@users.noreply.github.com>
2023-03-04 19:05:12 +0100

    Update translator.py

--- final_project/translator.py
+++ final_project/translator.py
@@ -9,3 +9,21 @@ language_translator = LanguageTranslatorV3(
 
 language_translator.set_service_url('https://api.au-syd.language-translator.watson.cloud.ibm.com/instances/572872b6-5ffc-408f-891f-939ec6bb0242')
 
+def translate_en_to_fr(self, text):
+    translation = self.language_translator.translate(
+        text=text,
+        source='en',
+        target='fr'
+    ).get_result()
+        
+    return translation['translations'][0]['translation']
+    
+def translate_fr_to_en(self, text):
+    translation = self.language_translator.translate(
+        text=text,
+        source='fr',
+        target='en'
+    ).get_result()
+        
+    return translation['translations'][0]['translation']
+
