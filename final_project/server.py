4e519e236716cd23663b1fe17b3efe22d8c68f6c
Enaouram <99275409+Enaouram@users.noreply.github.com>
2023-03-07 11:53:47 +0100

    Update server.py

--- final_project/server.py
+++ final_project/server.py
@@ -1,15 +1,28 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from translator import Translator
 
 app = Flask(__name__)
+translator = Translator()
 
 @app.route('/translate', methods=['POST'])
 def translate():
-    # implementation of the translate endpoint
-    input_text = request.json.get('input_text')
-    translation = # perform translation on input_text
-    return {'translation': translation}
+    # Get the request data as JSON
+    data = request.get_json()
 
-@app.route('/health')
-def health():
-    # implementation of the health endpoint
-    return {'status': 'OK'}
+    # Get the text to translate and the target language from the JSON data
+    text = data.get('text')
+    target_language = data.get('target_language')
+
+    # Translate the text using the Translator class
+    if target_language == 'fr':
+        translated_text = translator.translate_en_to_fr(text)
+    elif target_language == 'en':
+        translated_text = translator.translate_fr_to_en(text)
+    else:
+        return jsonify({'error': 'Invalid target language'}), 400
+
+    # Return the translated text as JSON
+    return jsonify({'text': translated_text}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
